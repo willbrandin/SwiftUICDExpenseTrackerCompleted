2a65fde7f62bf86944628dcf31154e64eb123259
Will Brandin <william.brandin@gmail.com>
2022-06-29 12:50:58 -0600

    Remove Identifiable conformance to fix build error

--- ExpenseTracker/Models/ExpenseLog+Extension.swift
+++ ExpenseTracker/Models/ExpenseLog+Extension.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-extension ExpenseLog: Identifiable {
+extension ExpenseLog {
     
     var categoryEnum: Category {
         Category(rawValue: category ?? "") ?? .other
